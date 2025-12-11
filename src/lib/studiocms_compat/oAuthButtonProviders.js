80ffa809e4e02ccbd6b415b10a85985204d4a8fc
Elias Ricardo <eeliasricardoo@gmail.com>
2025-12-11 09:21:59 -0500

    feat: Implement over 40 Spanish i18n improvements, optimize CTAs, and refine content logic.

--- src/lib/studiocms_compat/oAuthButtonProviders.js
+++ src/lib/studiocms_compat/oAuthButtonProviders.js
@@ -1,15 +1,10 @@
-import { StudioCMSRoutes } from "studiocms:lib";
-import { oAuthButtons } from "studiocms:plugins/auth/providers";
-const providerData = oAuthButtons.map(
-  ({ enabled, image, label, safeName }) => ({
-    enabled,
-    href: StudioCMSRoutes.authLinks.oAuthIndex(safeName),
-    label,
-    image
-  })
-);
+import { StudioCMSRoutes } from 'studiocms:lib';
+import { oAuthButtons } from 'studiocms:plugins/auth/providers';
+const providerData = oAuthButtons.map(({ enabled, image, label, safeName }) => ({
+	enabled,
+	href: StudioCMSRoutes.authLinks.oAuthIndex(safeName),
+	label,
+	image,
+}));
 const showOAuth = providerData.some((provider) => provider.enabled);
-export {
-  providerData,
-  showOAuth
-};
+export { providerData, showOAuth };
